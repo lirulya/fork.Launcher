9ae97d566ead14479ccc015df4e78f5506d9d8e9
Jordan Rey <rey.jordan.1998@gmail.com>
2023-05-07 09:05:36 +0200

    update appPackageUrl

--- .electron-builder.config.js
+++ .electron-builder.config.js
@@ -42,7 +42,7 @@ module.exports = async function () {
       createDesktopShortcut: "always",
       createStartMenuShortcut: true,
       artifactName: "ArenaReturnsLauncher-Setup.${ext}",
-      appPackageUrl: "https://launcher.arena-returns.com/download/latest",
+      appPackageUrl: "https://launcher.arena-returns.com/download/nsis",
     },
 
     // Linux build
